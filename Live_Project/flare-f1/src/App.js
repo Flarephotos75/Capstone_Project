163e9a4246c89d896feb8abe55293294962e907f
Steven Todd <steven@flare-photography.com>
2022-11-06 20:13:12 +0000

    Display Card parsing data to console and shows arrays

--- Live_Project/flare-f1/src/App.js
+++ Live_Project/flare-f1/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment}  from 'react';
+
+import GetTop5Drivers from './client/components/AllDriverDisplayCard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Fragment>
+      <div>
+        <h1>Flare-F1 Test Page</h1>
+        <GetTop5Drivers />
+      </div>
+   </Fragment>
   );
 }
 
